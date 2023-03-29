b13ee8dea8f39b3ed69a5417cecee8bc55b1d759
gohguanlin <36472333+gohguanlin@users.noreply.github.com>
2023-03-29 20:16:45 +0800

    fixed to read txt file instead

--- wordcloud_generator.py
+++ wordcloud_generator.py
@@ -5,10 +5,14 @@ import pandas as pd
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(
-    "/Users/damon/Documents/SIT/Y2T2/ICT2107/wordcount.csv", index_col=0)
+# df = pd.read_csv(
+#   "/Users/damon/Documents/SIT/Y2T2/ICT2107/wordcount.csv", index_col=0)
 
-word_dict = df.to_dict()['count']
+df = pd.read_csv(
+    "/Users/damon/Documents/SIT/Y2T2/ICT2107/wordcount_sampleoutput.txt", sep=",", usecols=['word', 'count'], names=['word', 'count'], header=0)
+print(df)
+
+word_dict = df.set_index('word')['count'].to_dict()
 
 wordcloud = WordCloud(width=800, height=400,
                       background_color='white').generate_from_frequencies(word_dict)
